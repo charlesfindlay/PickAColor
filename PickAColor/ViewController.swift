b42d05511b1fcadadcdbf82bf5444f219bb85ac7
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-09-22 14:54:57 -0400

    Setting up game

--- PickAColor/ViewController.swift
+++ PickAColor/ViewController.swift
@@ -10,14 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var timer = NSTimer()
+    var correctChoice = false
+    let colorWheel = ColorWheel()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        startGame()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func startGame() {
+        var computerChoice = colorWheel.randomColor()
+        
     }
 
 
