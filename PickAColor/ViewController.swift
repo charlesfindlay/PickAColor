3bcdb4d8ad6a9da21e90f0e96559f58251b311f5
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-09-22 15:28:10 -0400

    MVP game is working. Colors rotate. Stops on correct guess

--- PickAColor/ViewController.swift
+++ PickAColor/ViewController.swift
@@ -10,17 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var guessColorButton: UIButton!
+    @IBOutlet weak var resultUserGuess: UILabel!
+    
     
     var timer = NSTimer()
-    var correctChoice = false
     let colorWheel = ColorWheel()
+    var randomColor = UIColor()
+    var computerChoice = UIColor()
     
     
+    
+    override func viewWillAppear(animated: Bool) {
+        resultUserGuess.hidden = true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = colorWheel.randomColor()
+        randomColor = colorWheel.randomColor()
+        view.backgroundColor = randomColor
+        guessColorButton.tintColor = randomColor
+        guessColorButton.backgroundColor = UIColor.whiteColor()
+        
         startGame()
     }
 
@@ -29,19 +41,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
+    @IBAction func getUserGuess(sender: AnyObject) {
+        var currentColor = view.backgroundColor
+        
+        if(currentColor == computerChoice) {
+            timer.invalidate()
+            resultUserGuess.text = "You guessed correctly!"
+            resultUserGuess.hidden = false
+        } else {
+            resultUserGuess.text = "Wrong! Try Again."
+            resultUserGuess.hidden = false
+        }
+    }
+    
+    
     func startGame() {
-        var computerChoice = colorWheel.randomColor()
+        computerChoice = colorWheel.randomColor()
         
         timer = NSTimer.scheduledTimerWithTimeInterval(5.0, target: self, selector: Selector("changeBackgroundColor"), userInfo: nil, repeats: true)
         
     }
     
     func changeBackgroundColor() {
-        view.backgroundColor = colorWheel.randomColor()
+        randomColor = colorWheel.randomColor()
+        view.backgroundColor = randomColor
+        guessColorButton.tintColor = randomColor
+        resultUserGuess.hidden = true
         
-        if(correctChoice) {
-            timer.invalidate()
-        }
     }
 
 
